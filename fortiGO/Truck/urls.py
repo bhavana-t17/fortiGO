7ccebdeb1f3a7321f39d9da1870631671d89e909
hemanth-s17 <hemanth.s17@iiits.in>
2018-10-28 23:10:48 +0530

    Added SignUp page

--- fortiGO/Truck/urls.py
+++ fortiGO/Truck/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='Truck-home'),
+    path('signup/',views.signup,name = 'Truck-signup'),
+    path('register/',views.register,name = 'Truck-register')
 ]
--- fortiGO/Truck/views.py
+++ fortiGO/Truck/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import UserLog
+from django.contrib import messages
+#from Truck import signup
 # Create your views here.
 
 def home(request):
 	return render(request, 'Truck/base.html')
+def signup(request):
+	return render(request,'Truck/signup.html')
+
+def register(request):
+	print(request.POST)
+	email = request.POST['email']
+	password = request.POST['password']
+	name = request.POST['name']
+	pincode = request.POST['pincode']
+	if UserLog.objects.filter(email = email):
+		#messages.info(request,f'User Already Exists')
+		return render(request,'Truck/signup.html',)
+	userlog = UserLog.objects.create(email = email,password = password,name = name,pincode = pincode)
+	print("You are Registered Successfully")
+	return render(request,'Truck/base.html')
